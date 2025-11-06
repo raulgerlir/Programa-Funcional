bb9496fb1b761509cce1c1c5d424b4e76c257d5c
raulgerlir <raulgerlir@ciencias.unam.mx>
2025-11-05 19:23:27 -0600

    Update RandomNumb.java

--- RandomNumb.java
+++ RandomNumb.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 
 public class RandomNumb {
+    /**
+     * Ejecuta una función cada segundo un numero determinado de veces
+     */
     public static void repeatEverySecond(Runnable action) {
         int count = 0;
         while (count < 5) {
@@ -11,12 +14,16 @@ public class RandomNumb {
             count++;
         }
     }
-
+     /**
+     * Imprime un número aleatorio entre 0 y 99.
+     */
     public static void printRandomNumber() {
         Random r = new Random();
         System.out.println("Random number: " + r.nextInt(100));
     }
-
+     /**
+     * Metodo principal: pasa la función printRandomNumber como parametro
+     */
     static void main() {
         repeatEverySecond(RandomNumb::printRandomNumber);
     }
