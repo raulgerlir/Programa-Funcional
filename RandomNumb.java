3f407d1c325c9f1c831af53ddfea3e37e35bd30b
raulgerlir <raulgerlir@ciencias.unam.mx>
2025-11-05 19:15:49 -0600

    Update RandomNumb.java

--- RandomNumb.java
+++ RandomNumb.java
@@ -18,6 +18,6 @@ public class RandomNumb {
     }
 
     static void main() {
-        repeatEverySecond(RandomPrinter::printRandomNumber);
+        repeatEverySecond(RandomNumb::printRandomNumber);
     }
 }
